6140e8afc50dfd928b3df28ea9eccd6a03dea661
Joseph León <leonus96@gmail.com>
2017-04-12 14:46:42 -0500

    Maquetado HTMl del home y menu móvil

---
+++ public/js/source.js
@@ -0,0 +1,20 @@
+/**
+ * Created by joseph on 12/04/17.
+ */
+(()=>{
+	//--MOBILE MENU-->
+	const menu = document.querySelector('header nav'),
+		burgerButton = document.querySelector(".mobile-bar a");
+	let flag = true;
+	burgerButton.addEventListener('click', ()=>{
+		console.log('click');
+		if(flag){
+			menu.style.left = '0%';
+			flag = false;
+		}else {
+			menu.style.left = '-100%'
+			flag = true;
+		}
+
+	})
+})();
