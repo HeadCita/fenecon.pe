21aaef47460e68867f9e67eb5f9ceec5e0d92b06
Joseph León <leonus96@gmail.com>
2017-04-18 11:03:50 -0500

    Formulario de contacto y paquetes de cursos(AI) sin icono

--- controllers/page-controller.js
+++ controllers/page-controller.js
@@ -8,6 +8,29 @@ class PageController{
 	getUs(req, res, next){
 		res.render('nosotros');
 	}
+	//----COURSES-->
+	getCursoNuevoAi(req, res, next){
+		res.render('cursos/nuevos-ai');
+	}
+	getCursoRecategorizacion(req, res, next){
+		res.render('cursos/recategorizacion');
+	}
+	getCursoSoloManejo(req, res, next){
+		res.render('cursos/solo-manejo');
+	}
+	getCursoManejoDefensivo(req, res, next){
+		res.render('cursos/manejo-defensivo');
+	}
+	getCursoCamioneta(req, res, next){
+		res.render('cursos/camioneta');
+	}
+	getLocales(req, res, next){
+		res.render('locales');
+	}
+
+	getContacto(req, res, next){
+		res.render('contacto');
+	}
 }
 
 module.exports = PageController;
--- routes/page-router.js
+++ routes/page-router.js
@@ -10,6 +10,13 @@ const PageController = require('../controllers/page-controller'),
 
 router
 	.get('/', pc.getHome)
-	.get('/nosotros', pc.getUs);
+	.get('/nosotros', pc.getUs)
+	.get('/cursos/nuevos-ai', pc.getCursoNuevoAi)
+	.get('/cursos/recategorizacion', pc.getCursoRecategorizacion)
+	.get('/cursos/solo-manejo', pc.getCursoSoloManejo)
+	.get('/cursos/manejo-defensivo', pc.getCursoManejoDefensivo)
+	.get('/cursos/camioneta', pc.getCursoCamioneta)
+	.get('/locales', pc.getLocales)
+	.get('/contacto', pc.getContacto);
 
 module.exports = router;
