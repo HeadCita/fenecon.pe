63978a7158c8ee22f7927802483485fd76561ea1
Joseph León <leonus96@gmail.com>
2017-04-25 18:35:42 -0500

    Testimonios(Slider)

--- public/js/source.js
+++ public/js/source.js
@@ -2,18 +2,65 @@
  * Created by joseph on 12/04/17.
  */
 (() => {
-	//--MOBILE MENU-->
-	const menu = document.querySelector('header nav'),
-		burgerButton = document.querySelector(".mobile-bar span");
-	let flag = true;
-	burgerButton.addEventListener('click', () => {
-		if (flag) {
-			menu.style.left = '0%';
-			flag = false;
-		} else {
-			menu.style.left = '-100%'
-			flag = true;
-		}
 
-	})
+	/*---------Slider Testimonio------*/
+	const slider = document.querySelector('#slider'),
+		next = document.querySelector('#btn-next'),
+		prev = document.querySelector('#btn-prev');
+	console.log("hola");
+
+	slider.insertBefore(slider.lastElementChild, slider.firstElementChild);
+	slider.style.marginLeft="-33.3%";
+	let i=2;
+	console.log(slider.children[3]);
+	slider.children[i].classList.add("focus");
+
+	function testimonyRigth(){
+		let pos=33.3;
+		let anim = setInterval(animDerecha, 1);
+		slider.children[i].classList.remove("focus");
+		function animDerecha(){
+			if(pos >= 66.6){
+				slider.insertBefore(slider.firstElementChild,slider.lastElementChild.nextSibling);
+				slider.style.marginLeft = `-33.3%`;
+				slider.children[i].classList.add("focus");
+				clearInterval(anim)
+			}else{
+				pos= pos + 0.2;
+				slider.style.marginLeft = `-${pos}%`;
+			}
+		}
+	}
+
+	function testimonyLeft(){
+		let pos=33.3;
+		let anim = setInterval(animDerecha, 1);
+
+		function animDerecha(){
+			if(pos <= 0){
+				slider.children[i].classList.remove("focus");
+				slider.insertBefore(slider.lastElementChild, slider.firstElementChild);
+				slider.style.marginLeft = `-33.3%`;
+				slider.children[i].classList.add("focus");
+				clearInterval(anim)
+			}else{
+				pos = pos - 0.2;
+				slider.style.marginLeft = `-${pos}%`;
+			}
+		}
+	}
+
+	function testimonyAutoplay(){
+		setInterval(testimonyRigth, 3000)
+	}
+
+	testimonyAutoplay();
+
+	next.addEventListener('click', ()=>{
+		testimonyRigth();
+	});
+
+	prev.addEventListener('click', ()=>{
+		testimonyLeft();
+	});
 })();
