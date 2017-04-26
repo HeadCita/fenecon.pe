a0378beeec7704141cbebea3ab0a972b8d3bf460
Arnold Lopez <arnold426499@gmail.com>
2017-04-26 00:43:26 -0500

    Barra de navegacion - version mobil

--- public/js/source.js
+++ public/js/source.js
@@ -3,17 +3,27 @@
  */
 (() => {
 	//--MOBILE MENU-->
-	const menu = document.querySelector('header nav'),
-		burgerButton = document.querySelector(".mobile-bar span");
+	const menu = document.querySelector(".menu-items"),
+	 	  burgerButton = document.querySelector("#icon-bar");
 	let flag = true;
 	burgerButton.addEventListener('click', () => {
 		if (flag) {
-			menu.style.left = '0%';
+			menu.style.height = '100%';
 			flag = false;
 		} else {
-			menu.style.left = '-100%'
+			menu.style.height = '0';
 			flag = true;
 		}
 
 	})
 })();
+
+
+
+Math.easeInOutQuad = (t, b, c, d) => {
+        t /= d / 2;
+
+        if (t < 1) return c / 2 * t * t + b;
+        t--;
+        return -c / 2 * (t * (t - 2) - 1) + b;
+}
