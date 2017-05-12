6ae0784419b35c489a3987dbe5b9a88a301a0b94
Arnold Lopez <arnold426499@gmail.com>
2017-05-12 13:20:47 -0500

    cambio de background-color a #222 en submenus y cambios en package.json de server.js a global.js

--- public/js/source.js
+++ public/js/source.js
@@ -3,60 +3,7 @@
  */
 (() => {
 
-	/*---------Slider Testimonio------*/
-	const slider = document.querySelector('#slider'),
-		next = document.querySelector('#btn-next'),
-		prev = document.querySelector('#btn-prev');
-	console.log("hola");
-
-	slider.insertBefore(slider.lastElementChild, slider.firstElementChild);
-	slider.style.marginLeft="-33.3%";
-	let i=2;
-	console.log(slider.children[3]);
-	slider.children[i].classList.add("focus");
-
-	function testimonyRigth(){
-		let pos=33.3;
-		let anim = setInterval(animDerecha, 1);
-		slider.children[i].classList.remove("focus");
-		function animDerecha(){
-			if(pos >= 66.6){
-				slider.insertBefore(slider.firstElementChild,slider.lastElementChild.nextSibling);
-				slider.style.marginLeft = `-33.3%`;
-				slider.children[i].classList.add("focus");
-				clearInterval(anim)
-			}else{
-				pos= pos + 0.2;
-				slider.style.marginLeft = `-${pos}%`;
-			}
-        }
-    }
-
-	function testimonyLeft(){
-		let pos=33.3;
-		let anim = setInterval(animDerecha, 1);
-
-		function animDerecha(){
-			if(pos <= 0){
-				slider.children[i].classList.remove("focus");
-				slider.insertBefore(slider.lastElementChild, slider.firstElementChild);
-				slider.style.marginLeft = `-33.3%`;
-				slider.children[i].classList.add("focus");
-				clearInterval(anim)
-			}else{
-				pos = pos - 0.2;
-				slider.style.marginLeft = `-${pos}%`;
-			}
-		}
-	}
-
-	function testimonyAutoplay(){
-		setInterval(testimonyRigth, 7000)
-	}
-
-	testimonyAutoplay();
-
-	//Mapa no-scroll
+		//Mapa no-scroll
 	const mapContainers = Array.prototype.slice.apply(document.querySelectorAll('.contact-local .local.map'));		;
 	const maps = Array.prototype.slice.apply(document.querySelectorAll('.contact-local .local.map iframe'));
 	console.log(mapContainers);
@@ -73,6 +20,7 @@
 	mapContainers[1].addEventListener('mouseleave', () => {
 		maps[1].style.pointerEvents = 'none';
 	});
+
 
 /*	next.addEventListener('click', ()=>{
 		testimonyRigth();
