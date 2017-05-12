161becda0ef25086f1dbf85d32f69020b6fb51c8
Arnold Lopez <arnold426499@gmail.com>
2017-05-12 11:03:24 -0500

    Menu de secciones(rojo) y del home

--- public/js/source.js
+++ public/js/source.js
@@ -63,37 +63,85 @@
 	prev.addEventListener('click', ()=>{
 		testimonyLeft();
 	});*/
-    
-    //--MOBILE MENU-->
+
+	    //--MOBILE MENU-->
 	const menu = document.querySelector(".menu-items"),
 	 	  burgerButton = document.querySelector("#icon-bar"),
 		  listItem = document.getElementsByClassName("list-item");
 	let flag = true;
 	burgerButton.addEventListener('click', () => {
 		if (flag) {
-			menuHeight(menu, 300, 500);
+			menu.style.maxHeight = "1000px";
+			// menuHeight(menu, 350, 500);
 			flag = false;
-			listItem[0].classList.remove("hide");
-			listItem[1].classList.remove("hide");
-			listItem[2].classList.remove("hide");
-			listItem[3].classList.remove("hide");
-			listItem[4].classList.remove("hide");
-			listItem[5].classList.remove("hide");
-			listItem[6].classList.remove("hide");
 		} else {
-			menuHeight(menu, 0, 500);
-			listItem[0].classList.add("hide");
-			listItem[1].classList.add("hide");
-			listItem[2].classList.add("hide");
-			listItem[3].classList.add("hide");
-			listItem[4].classList.add("hide");
-			listItem[5].classList.add("hide");
-			listItem[6].classList.add("hide");
+			menu.style.maxHeight = 0;
+			// menuHeight(menu, 0, 500);
 			flag = true;
 		}
 	});
 
-})()
+
+	//--- SUB-MENU ----
+
+	const linkCursos = document.querySelector(".cursos"),
+			linkCategorias = document.querySelector(".categorias");
+	
+	let subMenuChild = element => {
+		for (let i = 0; i < element.childNodes.length; i++) {
+			if (element.childNodes[i].className == "sub-menu") {
+				return element.childNodes[i];
+			}
+		}
+	};
+	
+	let subMenu1 = subMenuChild(linkCursos);
+	let subMenu2 = subMenuChild(linkCategorias);
+
+	let mq = window.matchMedia( "(min-width: 1000px)" );
+	if(!mq.matches) {
+		menu.style.maxHeight = "0px";
+
+		let flagSubMenu = true;
+		linkCursos.addEventListener('click', () => {
+		console.log("prueba");
+			
+			if (flagSubMenu) {
+				subMenu1.style.left = 0;
+				linkCursos.style.backgroundColor = "#3a3838";
+				flagSubMenu = false;
+			} else {
+				linkCursos.style.backgroundColor = "rgb(34, 34, 34)";
+				subMenu1.style.left = '-100%';
+				flagSubMenu = true;
+			}
+		});
+
+		let flagSubMenu2 = true;
+		linkCategorias.addEventListener('click', () => {
+			
+			if (flagSubMenu2) {
+				linkCategorias.style.backgroundColor = "#3a3838";
+				subMenu2.style.left = 0;
+				flagSubMenu2 = false;
+			} else {
+				linkCategorias.style.backgroundColor = "rgb(34, 34, 34)";
+				subMenu2.style.left = '-100%';
+				flagSubMenu2 = true;
+			}
+		});
+
+	} else {
+		console.log(mq.matches);
+		subMenu1.style.left = "0";
+		subMenu2.style.left = "0";
+		menu.style.maxHeight = "1000px";
+	}
+
+
+})();
+
+
 
 
 function menuHeight(element, hasta, duracion) {
